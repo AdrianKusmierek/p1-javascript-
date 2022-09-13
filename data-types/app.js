67a0bbfd361d5da14516a50e4c713f599bb36e79
Adrian Kuśmierek <huskyusadoggy@gmail.com>
2022-09-13 12:04:50 +0200

    x

---
+++ data-types/app.js
@@ -0,0 +1,22 @@
+const myName = "Adrian Kuśmierek";
+const diploma = true;
+
+var myAge = 16;
+var passed;
+
+var num1 = 34.5;
+var num2 = 2;
+
+//keer operator
+var answer1 = num1 * num2;
+//gedeelt door operator
+var answer2 = num1 / num2;
+//plus operator
+var answer3 = num1 + num2;
+//minus operator
+var answer4 = num1 - num2;
+//tot de macht van operator
+var answer5 = num1 ** num2;
+
+document.write(`${myName}<br>${diploma}<br>${myAge}<br>${passed}<br>`);
+document.write(`${answer1}<br>${answer2}<br>${answer3}<br>${answer4}<br>${answer5}<br>`);
