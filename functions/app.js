43ffce3d84b16a20faf3c8d2d80026afda307142
Adrian Kuśmierek <huskyusadoggy@gmail.com>
2022-09-15 13:53:32 +0200

    x

--- functions/app.js
+++ functions/app.js
@@ -1,18 +1,7 @@
-var calculate = (x, y) => {
-    return x + y;
-}
-
-var multiplyByFive = x => {
-    return x * 5;
-}
-
-var checkInput = (...args) => {
-    return Math.max(...args);
-}
-
-var calculateMinutes = x => {
-    return x / 60;
-}
+var calculate = (x, y) => x + y;
+var multiplyByFive = x => x * 5;
+var checkInput = (...args) => Math.max(...args);
+var calculateMinutes = x => x / 60;
 
 alert(calculate(58, 11));
 alert(multiplyByFive(13.8))
