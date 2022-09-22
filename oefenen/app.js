fc02b799ff6902b38e8f1b78a4476f69019297a4
Adrian Kuśmierek <huskyusadoggy@gmail.com>
2022-09-22 14:00:13 +0200

    x

---
+++ oefenen/app.js
@@ -0,0 +1,37 @@
+/////////////////////////////////////////////////////////
+///                    Main Vars                      ///
+/////////////////////////////////////////////////////////
+
+// Defining the id within a variable for the ease of use.
+// Defining the title element from the index.html file.
+var idStr = "myId";
+var myElement = document.getElementById(idStr);
+var body = document.body;
+
+var toggle = false;
+
+/////////////////////////////////////////////////////////
+///                  Main Functions                   ///
+/////////////////////////////////////////////////////////
+
+function changeTitleColor() {
+    if (toggle) {
+        toggle = false;
+
+        myElement.style.display = "none";
+    } else {
+        toggle = true;
+
+        myElement.style.display = "block";
+    }
+}
+
+function hehe() {
+    body.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+}
+
+/////////////////////////////////////////////////////////
+///                       Main                        ///
+/////////////////////////////////////////////////////////
+
+myElement.style.fontWeight = "normal";
