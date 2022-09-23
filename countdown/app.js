3bb07701440c346727948bfdea603992c0d5a2df
Adrian Kuśmierek <huskyusadoggy@gmail.com>
2022-09-23 10:10:03 +0200

    uwu

---
+++ countdown/app.js
@@ -0,0 +1,50 @@
+///////////////////////////////////////////////
+///             Main Variables              ///
+///////////////////////////////////////////////
+
+// Defining the mandatory date variables.
+const countDate = new Date("2023-04-22T17:00:00");
+
+// Defining the h1 element from the index.html.
+var e = document.getElementById("counter");
+
+///////////////////////////////////////////////
+//               Main Functions             ///
+///////////////////////////////////////////////
+
+function countdown(targetDate) {
+    // Variables for setting the difference.
+    const now = new Date();
+    const gap = Math.abs(targetDate - now);
+
+    // Defining the overall time left.
+    const second = Math.ceil(gap / 1000);
+    const minute = Math.ceil(second / 60);
+    const hour = Math.ceil(minute / 60);
+    const day = Math.ceil(hour / 24);
+
+    // Defining the relative time left.
+    const textDay = `${day}`;
+    const textHour = `${hour % 24}`;
+    const textMinute = `${minute % 60}`;
+    const textSecond = `${second % 60}`;
+
+    // Returning the variables out of the funtion.
+    // As well as doing some nice formatting.
+    return `
+    ${(textDay < 10) ? `0${textDay}` : textDay} days\n
+    ${(textHour < 10) ? `0${textHour}` : textHour} hours\n
+    ${(textMinute < 10) ? `0${textMinute}` : textMinute} minutes\n
+    ${(textSecond < 10) ? `0${textSecond}` : textSecond} seconds
+    `;
+}
+
+///////////////////////////////////////////////
+//                   Main                   ///
+///////////////////////////////////////////////
+
+// Setting the time left on page load.
+e.innerText = countdown(countDate);
+
+// Updating the counter every 1000ms (1 second).
+setInterval(() => e.innerText = countdown(countDate), 1000);
