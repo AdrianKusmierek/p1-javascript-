69c2375a963b8fcf9abcf942c03ffce57eafd5c6
Adrian Kuśmierek <51207265+AdrianKusmierek@users.noreply.github.com>
2022-10-12 14:13:29 +0200

    Update app.js

--- opdracht_8.1/app.js
+++ opdracht_8.1/app.js
@@ -2,3 +2,5 @@ let multiply = (x, y, out) => out.innerText = x * y;
 let subtract = (x, y, out) => out.innerText = x - y;
 let add = (x, y, out) => out.innerText = x + y;
 let divide = (x, y, out) => out.innerText = x / y;
+
+// Ez peezy lemon fucking squeezy, vier lijnen
