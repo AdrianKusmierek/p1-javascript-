f61177ec8aeb1e828ccd5d5796eaa9a894f0e00c
Adrian Kuśmierek <51207265+AdrianKusmierek@users.noreply.github.com>
2022-10-12 14:08:21 +0200

    Add files via upload

---
+++ opdracht_8.1/app.js
@@ -0,0 +1,4 @@
+let multiply = (x, y, out) => out.innerText = x * y;
+let subtract = (x, y, out) => out.innerText = x - y;
+let add = (x, y, out) => out.innerText = x + y;
+let divide = (x, y, out) => out.innerText = x / y;
